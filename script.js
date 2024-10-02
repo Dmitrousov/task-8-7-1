0f418db6e71554c0480fc7ed28f4e0df9b75cde2
Dmitrousov <artem.dmitrousov@yandex.ru>
2024-10-02 13:07:43 +0500

    change script.js

--- script.js
+++ script.js
@@ -95,7 +95,7 @@ document.getElementById('btnLess').addEventListener('click', function () {
             answerField.innerText = answerPhrase;
             gameRun = false;
         } else {
-            minValue = answerNumber - 1;
+            maxValue = answerNumber - 1;
             answerNumber  = Math.floor((minValue + maxValue) / 2);
             orderNumber++;
             orderNumberField.innerText = orderNumber;
